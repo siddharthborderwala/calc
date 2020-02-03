10201ca4dd56f4476514de6762f6398430d9d032
siddharthborderwala <siddharthborderwala@users.noreply.github.com>
2020-02-03 13:21:54 +0530

    calculator is functional now

--- app.js
+++ app.js
@@ -1,57 +1,60 @@
 //LOGIC CONTROLLER
 const LGController = (function() {
 
+    return {
+        findVal: function(equation) {
+            return eval(equation);
+        }
+    }
 })();
 
 
 //UI CONTROLLER
 const UIController = (function() {
 
-    //set up click response event listeners
-    const setGeneralListeners = () => {
-        document.querySelectorAll('.btn').forEach(el => {
-            el.addEventListener('click', () => {
-                el.classList.add('click-transform');
-                setTimeout((el) => {
-                    el.classList.remove('click-transform');
-                }, 75, el);
-            });
-        });
-    };
+    const displayVal = document.querySelector('.display-value');
+    displayVal.textContent = '';
 
-    //set up listeners for numbers
-    const setNumListeners = () => {
-        document.querySelectorAll('.btn.num').forEach(el => {
-            el.addEventListener('click', () => {
-                console.log(parseInt(el.innerHTML.trim(), 10));
-            });
-        });
-    };
-
-    //set up listeners for operators
-    const setOperatorListeners = () => {
-        document.querySelectorAll('.btn.operator').forEach(el => {
-            el.addEventListener('click', () => {
-                console.log(`oper${el.id}`);
-            });
-        });
-    };
-
-    //set up listeners for back/ac
-    const setBackListeners = () => {
-        document.querySelectorAll('.btn.back').forEach(el => {
-            el.addEventListener('click', () => {
-                console.log(`back${el.id}`);
-            });
-        });
-    }
+    const displayEq = document.querySelector('.display-equation');
+    displayEq.textContent = '';
 
     return {
-        setListeners: () => {
-            setGeneralListeners();
-            setNumListeners();
-            setOperatorListeners();
-            setBackListeners();
+        btnClickEffect: function(e) {
+            let el = e.target.closest('.btn');
+            el.classList.add('click-transform');
+            setTimeout(el => {
+                el.classList.remove('click-transform');
+            }, 100, el);
+        },
+        nextNum: function(num) {
+            displayVal.textContent = displayVal.textContent.concat(num);
+        },
+        nextOper: function(oper) {
+            if (displayVal.textContent !== '' && oper !== '=') {
+                displayVal.textContent = displayVal.textContent.concat(oper);
+            }
+        },
+        backSpace: function(fn) {
+            if (fn === 'ac') {
+                displayVal.textContent = '';
+                displayEq.textContent = '';
+            } else {
+                displayVal.textContent = displayVal.textContent.slice(0, displayVal.textContent.length - 1);
+            }
+        },
+        eqEvent: function() {
+            const dispText = displayVal.textContent;
+            const len = dispText.length;
+            const condition = dispText[len - 1] === '/' || dispText[len - 1] === '*' || dispText[len - 1] === '+' || dispText[len - 1] === '-';
+            if (displayVal.textContent !== '' && !condition) {
+                displayEq.textContent = displayVal.textContent;
+                displayVal.textContent = '';
+            }
+            return displayEq.textContent;
+        },
+        updateVal: function(val) {
+            displayVal.textContent = '';
+            displayVal.textContent = `${val}`;
         }
     };
 
@@ -62,7 +65,43 @@ const UIController = (function() {
 const APPController = (function(uictrl, lgctrl) {
 
     const setUpEventListeners = () => {
-        uictrl.setListeners();
+
+        //set up click response event listeners
+        document.querySelectorAll('.btn').forEach(el => {
+            el.addEventListener('click', uictrl.btnClickEffect);
+        });
+
+
+        //set up listeners for numbers
+        document.querySelectorAll('.btn.num').forEach(el => {
+            el.addEventListener('click', () => {
+                const num = parseInt(el.innerHTML.trim(), 10);
+                uictrl.nextNum(num);
+            });
+        });
+
+        //set up listeners for operators
+        document.querySelectorAll('.btn.operator').forEach(el => {
+            el.addEventListener('click', () => {
+                const oper = el.getAttribute('data-value');
+                uictrl.nextOper(oper);
+            });
+        });
+
+        //set up listeners for back/ac
+        document.querySelectorAll('.btn.back').forEach(el => {
+            el.addEventListener('click', e => {
+                const el = e.target.closest('.btn');
+                uictrl.backSpace(el.id);
+            });
+        });
+
+        //listener for equal to
+        document.querySelector('#equal').addEventListener('click', e => {
+            const equation = uictrl.eqEvent();
+            const res = lgctrl.findVal(equation);
+            uictrl.updateVal(res);
+        });
     }
 
     return {
